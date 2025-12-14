c5a75c65bc2cb4db0c2f1cf79fdf8c9ae38646ba
Raiymbek Bexultanuly <raykin.2005@gmail.com>
2025-12-14 23:47:48 +0500

    yt-dlp is working but it downloads m4a

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-def main():
-    print("Hello from youtube-to-mp3!")
+import yt_dlp
 
+def main():
+    URLS = ['https://youtu.be/JNIK18PKrqY?si=Ycd1qCjYazf7_anv']
+
+    ydl_opts = {
+        'format': 'm4a/bestaudio/best',
+        # ℹ️ See help(yt_dlp.postprocessor) for a list of available Postprocessors and their arguments
+        'postprocessors': [{  # Extract audio using ffmpeg
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',  
+            #'ffmpeg_location': r'C:\Users\User\Desktop\Visual studio\youtube_to_mp3\ffmpeg-8.0.1',  
+        }]
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        error_code = ydl.download(URLS)
 
 if __name__ == "__main__":
     main()
