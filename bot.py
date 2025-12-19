626099a46d4de0f22289eca7fb83f337879cd12d
Raiymbek Bexultanuly <raykin.2005@gmail.com>
2025-12-19 11:42:32 +0500

    main.py downloads mp3, bot.py is basic

--- bot.py
+++ bot.py
@@ -1,6 +1,24 @@
 from dotenv import load_dotenv
 import os
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 
 load_dotenv()
 BOT_TOKEN = os.getenv("TG_BOT_TOKEN")
 BOT_USERNAME = '@youtube_to_mp3_downloadBot'
+
+# Commands
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text('Hello! Send your youtube video)')
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text('Just copy the link (URL) of the video and send it here!')
+
+async def custom_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text('This is custom command')
+
+
+# Responses
+
+# def handle_respones(text: str) -> str:
+    
--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import yt_dlp
 
 def main():
-    URLS = ['https://youtu.be/JNIK18PKrqY?si=Ycd1qCjYazf7_anv']
+    URLS = ['https://youtu.be/O0Cw1SLdxxE?si=VM-2SK2tbzDDvAu4']
 
     ydl_opts = {
-        'format': 'm4a/bestaudio/best',
-        # ℹ️ See help(yt_dlp.postprocessor) for a list of available Postprocessors and their arguments
-        'postprocessors': [{  # Extract audio using ffmpeg
+        'format': 'bestaudio/best',
+        'postprocessors': [{
             'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',  
-            #'ffmpeg_location': r'C:\Users\User\Desktop\Visual studio\youtube_to_mp3\ffmpeg-8.0.1',  
-        }]
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
     }
 
+
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        error_code = ydl.download(URLS)
+        ydl.download(URLS)
 
 if __name__ == "__main__":
     main()
